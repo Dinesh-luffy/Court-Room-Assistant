5d8c6f40edd1c7b2dd8fe566cf759c8485f4d143
Dinesh Kumar S <dineshkumardk2k6@gmail.com>
2025-12-04 09:43:01 +0530

    added core db setupt file

---
+++ core.py
@@ -0,0 +1,16 @@
+# setup_core_db.py
+import os
+from rag import load_pdf_text, chunk_text, store_in_faiss
+from main import BASE_DB_PATH
+
+CORE_DB_PATH = os.path.join(BASE_DB_PATH, 'core_knowledge')
+PDF_PATH = "data/core_knowledge_base/ipc_book.pdf"
+
+if not os.path.exists(PDF_PATH):
+    print(f"⚠️ Error: The file {PDF_PATH} was not found.")
+else:
+    print("⏳ Starting to process the core legal database...")
+    text = load_pdf_text(PDF_PATH)
+    chunks = chunk_text(text)
+    store_in_faiss(chunks, CORE_DB_PATH)
+    print("✅ Core legal database setup is complete.")
