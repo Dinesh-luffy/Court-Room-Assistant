fcb6525d90703b23f2024ebc4e66294c55e3661a
Dinesh Kumar S <dineshkumardk2k6@gmail.com>
2025-12-08 23:17:35 +0530

    added core.py

--- core.py
+++ core.py
@@ -7,10 +7,10 @@ CORE_DB_PATH = os.path.join(BASE_DB_PATH, 'core_knowledge')
 PDF_PATH = "data/core_knowledge_base/ipc_book.pdf"
 
 if not os.path.exists(PDF_PATH):
-    print(f"⚠️ Error: The file {PDF_PATH} was not found.")
+    print(f" Error: The file {PDF_PATH} was not found.")
 else:
     print("⏳ Starting to process the core legal database...")
     text = load_pdf_text(PDF_PATH)
     chunks = chunk_text(text)
     store_in_faiss(chunks, CORE_DB_PATH)
-    print("✅ Core legal database setup is complete.")
+    print(" Core legal database setup is complete.")
